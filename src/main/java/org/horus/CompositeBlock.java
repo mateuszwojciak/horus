77252262c1e5214c4ac853a7e7bf00a1c9eee04d
Mateusz Wójciak <matwojciak@gmail.com>
2022-09-07 21:38:54 +0200

    implement find methods and count method

--- src/main/java/org/horus/CompositeBlock.java
+++ src/main/java/org/horus/CompositeBlock.java
@@ -3,5 +3,5 @@ package org.horus;
 import java.util.List;
 
 public interface CompositeBlock extends Block {
-    List getBlocks();
+    List<Block> getBlocks();
 }
--- src/main/java/org/horus/Wall.java
+++ src/main/java/org/horus/Wall.java
@@ -1,7 +1,60 @@
 package org.horus;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Wall implements Structure {
-    private List blocks;
+    private List<Block> blocks;
+
+    public Wall(List<Block> blocks) {
+        this.blocks = blocks;
+    }
+
+    @Override
+    public Optional<Block> findBlockByColor(String color) {
+
+        return blocks
+                .stream()
+                .flatMap(s -> getAllBlocks(s).stream())
+                .filter(s -> s.getColor().equalsIgnoreCase(color))
+                .findAny();
+    }
+
+    @Override
+    public List<Block> findBlocksByMaterial(String material) {
+
+        return blocks
+                .stream()
+                .flatMap(s -> getAllBlocks(s).stream())
+                .filter(s -> s.getMaterial().equalsIgnoreCase(material))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public int count() {
+
+        return blocks
+                .stream()
+                .mapToInt(s -> getAllBlocks(s).size())
+                .sum();
+    }
+
+    private List<Block> getAllBlocks(Block block) {
+        List<Block> listWithAllBlocks = new ArrayList<>();
+        listWithAllBlocks.add(block);
+
+        if (block instanceof CompositeBlock) {
+            List<Block> children = ((CompositeBlock) block)
+                    .getBlocks()
+                    .stream()
+                    .flatMap(s -> getAllBlocks(s).stream())
+                    .collect(Collectors.toList());
+
+            listWithAllBlocks.addAll(children);
+        }
+
+        return listWithAllBlocks;
+    }
 }
